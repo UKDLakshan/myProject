4ddfd107b7c4834496e841c15be5f61f92eab81a
Lochana108 <deshanlochana2@gmail.com>
2024-10-03 00:29:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,475 @@
+let items = [
+    {
+        "itemCode": "B1001",
+        "itemName": "Classic Burger (Large)",
+        "price": "750.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1002",
+        "itemName": "Classic Burger (Regular) ",
+        "price": "1500.00",
+        "discount": "15",
+        image: "img/burger-main.png"
+    }, {
+        "itemCode": "B1003",
+        "itemName": "Turkey Burger",
+        "price": "1600.00",
+        "discount": null,
+        image: "img/close-up-burger-with-fries-plate.jpg"
+    },
+    {
+        "itemCode": "B1004",
+        "itemName": "Chicken Burger (Large) ",
+        "price": "1400.00",
+        "discount": null,
+        image: "img/front-view-burger-with-veggies-meat.jpg"
+    },
+    {
+        "itemCode": "B1005",
+        "itemName": "Chicken Burger (Regular) ",
+        "price": "800.00",
+        "discount": "20",
+        image: "img/bu-bu.jpg"
+    },
+    {
+        "itemCode": "B1006",
+        "itemName": "Cheese Burger (Large)",
+        "price": "1000.00",
+        "discount": null,
+        image: "img/still-life-delicious-american-hamburger.jpg"
+    },
+    {
+        "itemCode": "B1007",
+        "itemName": "Cheese Burger (Regular) ",
+        "price": "600.00",
+        "discount": null,
+        image: "img/big-sandwich.jpg"
+    },
+    {
+        "itemCode": "B1008",
+        "itemName": "Bacon Burger ",
+        "price": "650.00",
+        "discount": "15",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1009",
+        "itemName": "Shawarma Burger",
+        "price": "800.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1010",
+        "itemName": "Olive Burger ",
+        "price": "1800.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    }, {
+        "itemCode": "B1012",
+        "itemName": "Double-Cheese Burger",
+        "price": "1250.00",
+        "discount": "20",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1013",
+        "itemName": "Crispy Chicken Burger (Regular)   ",
+        "price": "1200.00",
+        "discount": "20",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1014",
+        "itemName": "Crispy Chicken Burger (Large) ",
+        "price": "1600.00",
+        "discount": "10",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1015",
+        "itemName": "Paneer Burger ",
+        "price": "900.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1016",
+        "itemName": "Crispy Chicken Submarine (Large) ",
+        "price": "2000.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1017",
+        "itemName": "Crispy Chicken Submarine (Regular) ",
+        "price": "1500.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    }, {
+        "itemCode": "B1018",
+        "itemName": "Chicken Submarine (Large)",
+        "price": "1800.00",
+        "discount": "3",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1019",
+        "itemName": "Chicken Submarine (Regular) ",
+        "price": "1400.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    }, {
+        "itemCode": "B1020",
+        "itemName": "Grinder Submarine ",
+        "price": "2300.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1021",
+        "itemName": "Cheese Submarine ",
+        "price": "2200.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1022",
+        "itemName": "Double Cheese n Chicken Submarine  ",
+        "price": "1900.00",
+        "discount": "16",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1023",
+        "itemName": "Special Horgie Submarine ",
+        "price": "2800.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1024",
+        "itemName": "MOS Special Submarine ",
+        "price": "3000.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1025",
+        "itemName": "Steak Fries (Large)",
+        "price": "1200.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1026",
+        "itemName": "Steak Fries (Medium)",
+        "price": "600.00",
+        "discount": null,
+        image: "img/sandwich-with-chicken-fries.jpg"
+    },
+    {
+        "itemCode": "B1027",
+        "itemName": "French Fries (Large) ",
+        "price": "800.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    }, {
+        "itemCode": "B1028",
+        "itemName": "French Fries (Medium)",
+        "price": "650.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1029",
+        "itemName": "French Fries (Small) ",
+        "price": "450.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1030",
+        "itemName": "Sweet Potato Fries (Large) ",
+        "price": "600.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1031",
+        "itemName": "Chicken n Cheese Pasta ",
+        "price": "1600.00",
+        "discount": "15",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1032",
+        "itemName": "Chicken Penne Pasta  ",
+        "price": "1700.00 ",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1033",
+        "itemName": "Ground Turkey Pasta Bake ",
+        "price": "2900.00",
+        "discount": "10",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1034",
+        "itemName": "Creamy Shrimp Pasta",
+        "price": "2000.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1035",
+        "itemName": "Lemon Butter Pasta",
+        "price": "1950.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1036",
+        "itemName": "Tagliatelle Pasta ",
+        "price": "2400.00",
+        "discount": "1",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1037",
+        "itemName": "Baked Ravioli ",
+        "price": "2000.00",
+        "discount": "1",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1038",
+        "itemName": "Fried Chicken (Small) ",
+        "price": "1200.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    }, {
+        "itemCode": "B1039",
+        "itemName": "Fried Chicken (Regular)",
+        "price": "2300.00",
+        "discount": "10",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1040",
+        "itemName": "Fried Chicken (Large) ",
+        "price": "3100.00",
+        "discount": "5",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1041",
+        "itemName": "Hot Wings (Large) ",
+        "price": "2400.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1042",
+        "itemName": "Devilled Chicken (Large)  ",
+        "price": "900.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1043",
+        "itemName": "BBQ Chicken (Regular) ",
+        "price": "2100.00 ",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1044",
+        "itemName": "Pepsi (330ml)",
+        "price": "990.00",
+        "discount": "5",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1045",
+        "itemName": "Coca-Cola (330ml) ",
+        "price": "1230.00",
+        "discount": null,
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1046",
+        "itemName": "Sprite (330ml) ",
+        "price": "1500.00",
+        "discount": "3",
+        image: "img/burger-main2.png"
+    },
+    {
+        "itemCode": "B1047",
+        "itemName": "Mirinda (330ml)",
+        "price": "850.00",
+        "discount": "7",
+        image: "img/burger-main2.png"
+    }
+];
+let customers = [];
+const allproduct = [...new Set(items.map((item) => { return item }))]
+let i = 0;
+document.getElementById('root').innerHTML = allproduct.map((item) => {
+    var { image, itemName, price, discount } = item;
+    return (
+        `<div class = 'box'>
+            <div class = 'img-box'>
+                <img class = 'images' src = ${image}></img>
+            </div>
+            <div class ='bottom'>
+            <p>${itemName}</p>
+            <h2>$${price}&nbsp&nbsp&nbsp${discount}%</h2>` +
+        "<button onclick = 'addtocart(" + (i++) + ")'> Add to Cart </button>" +
+        `</div>
+        </div>`
+    )
+}).join('')
+let cart = [];
+function addtocart(c) {
+    cart.push({ ...allproduct[c] });
+    cartDisplay();
+}
+function deleteElement(c) {
+    cart.splice(c, 1);
+    cartDisplay();
+}
+function cartDisplay(c) {
+    let j = 0, total = 0;
+    document.getElementById("count").innerHTML = cart.length;
+    if (cart.length == 0) {
+        document.getElementById('cartItem').innerHTML = "empty";
+        document.getElementById("total").innerHTML = "$ " + 0;
+
+    } else {
+        document.getElementById("cartItem").innerHTML = cart.map((items) => {
+            var { image, itemName, price, discount } = items;
+            let dis;
+            if(discount!==null){
+                dis =  parseInt(discount);
+            }else{
+                dis=0;
+            }
+            
+            total = total + parseFloat(price) - (price*(dis/100));
+            document.getElementById("total").innerHTML = "$ " + total;
+            return (
+                `<div class ='cart-item'>
+            <div class = 'row-img'>
+                <img class = 'rowimg' src = ${image}>
+            </div>
+            <p style = 'font-size:10px;'>${itemName}</p>
+            <h2 style = 'font-size: 15px;'>$ ${price}&nbsp&nbsp${discount}%</h2>` +
+                "<p class='del' onclick ='deleteElement(" + (j++) + ")'>del</p></div>"
+            );
+
+        }).join('');
+    }
+}
+
+function addItem() {
+    const itemName = document.getElementById("itemName").value;
+    const itemCode = document.getElementById("itemCode").value;
+    const price = parseFloat(document.getElementById("price").value);
+    const discount = parseInt(document.getElementById("discount").value);
+    const newItem = {
+        itemCode,
+        itemName,
+        price,
+        discount
+    };
+    items.push(newItem);
+    alert("Item added successfully!");
+    viewItems();
+    console.log(items);
+}
+
+function viewItems() {
+
+    const itemTableBody = document.getElementById("itemTableBody");
+    itemTableBody.innerHTML = '';
+    items.forEach((item) => {
+
+        const row = document.createElement("tr");
+
+
+        row.innerHTML = `
+                <td>${item.itemCode}</td>
+                <td>${item.itemName}</td>
+                <td>${item.price} Rs/=</td>
+                <td>${item.discount} %</td>
+                <td>
+                    <button class = "btn btn-outline-danger" onclick="deleteItem('${item.itemCode}')">Delete</button>
+                    <button class="btn btn-outline-success" onclick="updateItem('${item.itemCode}')">Update</button>
+                </td>
+            `;
+
+
+        itemTableBody.appendChild(row);
+
+    });
+}
+
+function deleteItem(itemCode) {
+    items = items.filter(item => item.itemCode !== itemCode);
+    alert("item deleted successfully");
+    viewItems();
+}
+
+function updateItem(itemCode) {
+    const item = items.find(i => i.itemCode === itemCode);
+    if (item) {
+
+        document.getElementById("itemName").value = item.itemName;
+        document.getElementById("itemCode").value = item.itemCode;
+        document.getElementById("price").value = item.price;
+        document.getElementById("discount").value = item.discount;
+
+
+        const addButton = document.querySelector("button[onclick='addItem()']");
+        addButton.innerText = "Update";
+        addButton.onclick = function () {
+            itemtemChangesSave(itemCode);
+
+        };
+    } else {
+        alert("Item not found!");
+    }
+}
+
+
+function itemtemChangesSave(itemCode) {
+    const itemIndex = items.findIndex(item => item.itemCode === itemCode);
+    if (itemIndex !== -1) {
+        items[itemIndex].itemName = document.getElementById("itemName").value;
+        items[itemIndex].price = document.getElementById("price").value;
+        items[itemIndex].discount = document.getElementById("discount").value;
+
+        alert("Item updated successfully!");
+        viewItems();
+
+        document.getElementById("itemName").value = '';
+        document.getElementById("itemCode").value = '';
+        document.getElementById("price").value = '';
+        document.getElementById("discount").value = '';
+
+        const addButton = document.querySelector("#add-btn");
+        addButton.innerText = "Add Item";
+        addButton.onclick = addItem;
+
+
+
+    } else {
+        alert("Item not found!");
+    }
+}
+
